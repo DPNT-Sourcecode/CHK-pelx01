e37d0d036746b80f468812b367ee02326a44e5f0
sbx_user1051 <sbx_user1051@169.254.57.182>
2024-12-23 16:16:32 +0000

    Mon Dec 23 16:16:32 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -15,16 +15,26 @@ def checkout(skus):
         "D":15,
     }
 
+    special_offers = {
+        "A": (3, 130),
+        "B": (2, 45),
+    }
+
     item_counts = {k: v for k, v in Counter(skus).items() if k in price_list}
     
-
-
     print(price_list)
     print(item_counts)
 
+    total_price = 0
+
     for item in item_counts:
+        if item in special_offers:
+            offer_quanity, offer_price = special_offers[item]
+            eligible_offers = item_counts[item] // offer_quanity
+            print (eligible_offers)
+
         total_item_count = price_list[item] * item_counts[item]
-        print(total_item_count)
+        # print(total_item_count)
     # print(skus)
 
 checkout("ABCDCBAABCABBAAAZZZ")
