1f85a356dc4862785154748f01230f5a1309ae26
sbx_user1051 <sbx_user1051@169.254.57.182>
2024-12-23 16:10:32 +0000

    Mon Dec 23 16:10:32 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -9,14 +9,22 @@ from collections import Counter
 def checkout(skus):
 
     price_list = {
-        "A":"50",
-        "B":"30",
-        "C":"20",
-        "D":"15",
+        "A":50,
+        "B":30,
+        "C":20,
+        "D":15,
     }
 
     item_counts = {k: v for k, v in Counter(skus).items() if k in price_list}
+    
 
+
+    print(price_list)
+    print(item_counts)
+
+    for item in item_counts:
+        total_item_count = price_list[item] * item_counts[item]
+        print(total_item_count)
     # print(skus)
 
 checkout("ABCDCBAABCABBAAAZZZ")
