ce717351349fa048b7c74ee32b398f1f63216c63
sbx_user1051 <sbx_user1051@169.254.57.182>
2024-12-23 15:48:49 +0000

    Mon Dec 23 15:48:49 UTC 2024

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -1,6 +1,3 @@
 # noinspection PyShadowingBuiltins,PyUnusedLocal
 def compute(x, y):
-    if x == True and y == True:
-        return x+y
-    else:
-        print("Please enter valid numbers")
+    return x+y
